2f282dafe99b0da186efa3b7eac02eeeb3573e95
lduarte <lduarte@localhost>
2013-08-26 14:33:17 -0300

    Primeiro formul?rio de Cadastro de Livro

--- web/src/main/java/br/com/maps/labrador/LabradorMenuCreator.java
+++ web/src/main/java/br/com/maps/labrador/LabradorMenuCreator.java
@@ -1,15 +1,15 @@
 package br.com.maps.labrador;
 
-import br.com.maps.labrador.pages.AlterarSenhaPage;
-import br.com.maps.labrador.pages.CrudUsuarioPage;
 import jmine.tec.web.pages.datadigester.importacao.Importacao;
 import jmine.tec.web.pages.persist.audit.ConsultaTrilhaAuditoria;
 import jmine.tec.web.pages.persist.auth.Authorization;
 import jmine.tec.web.pages.rtm.ConsultaExceptionRecord;
 import jmine.tec.web.pages.rtm.diagnosticador.ConsultaDiagnosticador;
-
 import jmine.tec.web.wicket.component.menu.cfg.AbstractMenuConfigFactoryBean;
 import jmine.tec.web.wicket.component.menu.cfg.MenuConfig;
+import br.com.maps.labrador.pages.AlterarSenhaPage;
+import br.com.maps.labrador.pages.CrudUsuarioPage;
+import br.com.maps.labrador.pages.cadastro.livro.CadastroLivro;
 
 /**
  * Starting point menu creator
@@ -26,6 +26,7 @@ public class LabradorMenuCreator extends AbstractMenuConfigFactoryBean {
         MenuConfig config = new MenuConfig();
 
         // add menu here
+        config.addPage(CadastroLivro.class, "Cadastros", "Livros");
         config.addPage(Authorization.class, "Autorização", "Autorizar");
         config.addPage(ConsultaTrilhaAuditoria.class, "Autorização", "Auditoria");
         config.addPage(ConsultaDiagnosticador.class, "Infra", "Diagnosticador");
@@ -33,7 +34,7 @@ public class LabradorMenuCreator extends AbstractMenuConfigFactoryBean {
         config.addPage(Importacao.class, "Infra", "Importacao");
         config.addPage(AlterarSenhaPage.class, "Infra", "Alterar senha");
         config.addPage(CrudUsuarioPage.class, "Infra", "Controle de acesso");
-        
+
         return config;
     }
 
