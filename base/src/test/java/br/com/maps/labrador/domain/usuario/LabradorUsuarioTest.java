4fcacc0fe439483f15259725bd6a3f137fdf2382
Luis Felipe Volpato <finxster@users.noreply.github.com>
2013-09-06 15:31:18 -0300

    corrigindo testes #4

--- base/src/test/java/br/com/maps/labrador/domain/usuario/LabradorUsuarioTest.java
+++ base/src/test/java/br/com/maps/labrador/domain/usuario/LabradorUsuarioTest.java
@@ -9,6 +9,8 @@ public class LabradorUsuarioTest extends BOBaseTestCase<LabradorUsuario> {
 
     private static User[] USER;
 
+    private static String[] NOME;
+
     private static String[] EMAIL;
 
     /**
@@ -17,6 +19,7 @@ public class LabradorUsuarioTest extends BOBaseTestCase<LabradorUsuario> {
     @Override
     protected void initializeTestData() throws BeanNotFoundException {
         USER = new UserTest().getSavedTestData().toArray(new User[2]);
+        NOME = new String[]{ "nome1", "nome2" };
         EMAIL = new String[]{ "usuario1@maps.com.br", "usuario2@maps.com.br" };
     }
 
@@ -26,6 +29,7 @@ public class LabradorUsuarioTest extends BOBaseTestCase<LabradorUsuario> {
     @Override
     protected void fillData(int idx, LabradorUsuario bo) {
         bo.setUser(USER[idx]);
+        bo.setNome(NOME[idx]);
         bo.setEmail(EMAIL[idx]);
     }
 
@@ -35,6 +39,7 @@ public class LabradorUsuarioTest extends BOBaseTestCase<LabradorUsuario> {
     @Override
     protected void compareData(int idx, LabradorUsuario labradorUsuario) throws BeanNotFoundException {
         assertEquals(labradorUsuario.getUser(), USER[idx]);
+        assertEquals(labradorUsuario.getNome(), NOME[idx]);
         assertEquals(labradorUsuario.getEmail(), EMAIL[idx]);
         assertSame(labradorUsuario.getUserName(), labradorUsuario.getUser().getUsername());
     }
