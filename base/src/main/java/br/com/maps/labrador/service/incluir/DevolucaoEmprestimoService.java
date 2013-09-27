13d2cea17a0b51e8e9099288ba5ca46ce3cfd842
laercio.duarte <laercio.tecnologia@gmail.com>
2013-09-27 02:13:57 -0300

    Servi?o de inclus?o de devolu??o de emprestimo e algumas planilhas de testes.

---
+++ base/src/main/java/br/com/maps/labrador/service/incluir/DevolucaoEmprestimoService.java
@@ -0,0 +1,110 @@
+package br.com.maps.labrador.service.incluir;
+
+import jmine.tec.di.annotation.Injected;
+import jmine.tec.persist.api.DAO;
+import jmine.tec.persist.api.persister.StatelessPersister;
+import jmine.tec.services.api.ActionsEnum;
+import jmine.tec.services.api.annotations.Execution;
+import jmine.tec.services.api.annotations.Input;
+import jmine.tec.services.api.annotations.Output;
+import jmine.tec.services.api.annotations.ServiceImplementor;
+import jmine.tec.utils.date.Clock;
+import jmine.tec.utils.date.Date;
+import br.com.maps.labrador.domain.emprestavel.AbstractEmprestavel;
+import br.com.maps.labrador.domain.emprestimo.Emprestimo;
+import br.com.maps.labrador.domain.usuario.LabradorUsuario;
+
+/**
+ * Serviço para {@link CadastroDevolucaoEmprestimo} de {@link Emprestimo}
+ * 
+ * @author laercio.duarte
+ * @created Sep 27, 2013
+ */
+@ServiceImplementor(action = ActionsEnum.INCLUIR)
+public class DevolucaoEmprestimoService {
+
+    private static final String IDENTIFICADOR = "Identificador";
+
+    private static final String COISA = "Coisa";
+
+    private static final String DATA_DEVOLUCAO = "Data Devolução";
+
+    private static final String LABRADOR_USUARIO = "Tomador";
+
+    private AbstractEmprestavel coisa;
+
+    private Date dataDevolucao;
+
+    private DAO<Emprestimo> dao;
+
+    private LabradorUsuario tomador;
+
+    private Clock clock;
+
+    private StatelessPersister<Emprestimo> persister;
+
+    /**
+     * Execução do serviço de cadastro de {@link CadastroDevolucaoEmprestimo}
+     * 
+     * @return {@link Emprestimo}
+     */
+    @Execution
+    @Output(propertyName = IDENTIFICADOR)
+    public Emprestimo execute() {
+        Emprestimo emprestimo = this.dao.createBean();
+        emprestimo.setEmprestavel(this.coisa);
+        emprestimo.setData(this.clock.currentTimestamp());
+        emprestimo.setDataDevolucao(this.dataDevolucao);
+        emprestimo.setTomador(this.tomador);
+        this.persister.save(emprestimo);
+        return emprestimo;
+    }
+
+    /**
+     * @param coisa the coisa to set
+     */
+    @Input(fieldName = COISA)
+    public void setCoisa(AbstractEmprestavel coisa) {
+        this.coisa = coisa;
+    }
+
+    /**
+     * @param dataDevolucao the dataDevolucao to set
+     */
+    @Input(fieldName = DATA_DEVOLUCAO)
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    /**
+     * @param labradorUsuario the labradorUsuario to set
+     */
+    @Input(fieldName = LABRADOR_USUARIO)
+    public void setLabradorUsuario(LabradorUsuario labradorUsuario) {
+        this.tomador = labradorUsuario;
+    }
+
+    /**
+     * @param clock the clock to set
+     */
+    @Injected
+    public void setClock(Clock clock) {
+        this.clock = clock;
+    }
+
+    /**
+     * @param dao the dao to set
+     */
+    @Injected
+    public void setDao(DAO<Emprestimo> dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * @param persister the persister to set
+     */
+    @Injected
+    public void setPersister(StatelessPersister<Emprestimo> persister) {
+        this.persister = persister;
+    }
+}
