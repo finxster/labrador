09b1196401718a559dd2f828d11976c732385be5
luis.volpato <luis.volpato@localhost>
2013-10-03 20:30:20 -0300

    primeira versao com modal

--- web/src/main/java/br/com/maps/labrador/pages/main/CadastrosPanel.java
+++ web/src/main/java/br/com/maps/labrador/pages/main/CadastrosPanel.java
@@ -1,10 +1,12 @@
 package br.com.maps.labrador.pages.main;
 
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.ImageButton;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.resource.PackageResourceReference;
+
+import de.agilecoders.wicket.core.markup.html.bootstrap.dialog.Modal;
 
 /**
  * Painel utilizado para selecionar qual objeto vai ser cadastrado.
@@ -22,9 +24,32 @@ public class CadastrosPanel extends Panel {
     protected void onInitialize() {
         super.onInitialize();
 
-        this.add(new Label("livros", Model.of("Livros!")).setOutputMarkupId(true));
-        this.add(new Label("equipamentos", Model.of("Equipamentos!")).setOutputMarkupId(true));
-        this.add(new Label("jogos", Model.of("Jogos!")).setOutputMarkupId(true));
+        final Modal modal = new Modal("modalLivros");
+        this.add(modal);
+        final ImageButton imageButton = new ImageButton("livros", new PackageResourceReference("images/Livros.png")) {
+        };
+        imageButton.add(new AjaxEventBehavior("onclick") {
+
+            @Override
+            protected void onEvent(AjaxRequestTarget target) {
+                modal.appendShowDialogJavaScript(target);
+            }
+        });
+        // imageButton.add(new AjaxEventBehavior("onmouseup") {
+        //
+        // @Override
+        // protected void onEvent(AjaxRequestTarget target) {
+        // imageButton.add(AttributeModifier.append("src", "images/Livros_selecionado.png"));
+        // }
+        // });
+
+        this.add(imageButton);
+        this.add(new ImageButton("equipamentos", new PackageResourceReference("images/Equipamentos.png")));
+        this.add(new ImageButton("jogos", new PackageResourceReference("images/Videogames.png")));
+
+        // this.add(new Label("livros", Model.of("Livros!")).setOutputMarkupId(true));
+        // this.add(new Label("equipamentos", Model.of("Equipamentos!")).setOutputMarkupId(true));
+        // this.add(new Label("jogos", Model.of("Jogos!")).setOutputMarkupId(true));
 
     }
 
