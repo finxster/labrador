24eca647057dd8360d6261cfe2085a20c6f52489
lduarte <laercio.tecnologia@gmail.com>
2013-10-23 16:26:19 -0200

    Servi?o de exclus?o abstrato para objetos de atributos iguais

--- base/src/main/java/br/com/maps/labrador/dao/emprestavel/AbstractEmprestavelDAO.java
+++ base/src/main/java/br/com/maps/labrador/dao/emprestavel/AbstractEmprestavelDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jmine.tec.persist.api.dao.BeanNotFoundException;
 import jmine.tec.persist.impl.dao.BaseDAO;
+import jmine.tec.persist.impl.hibernate.RestrictionsUtils;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
@@ -11,12 +12,18 @@ import org.hibernate.criterion.Restrictions;
 import br.com.maps.labrador.domain.emprestavel.AbstractEmprestavel;
 import br.com.maps.labrador.domain.usuario.LabradorUsuario;
 
+/**
+ * DAO para {@link AbstractEmprestavel}.
+ * 
+ * @author laercio.duarte
+ * @created Aug 29, 2013
+ */
 public class AbstractEmprestavelDAO extends BaseDAO<AbstractEmprestavel> {
 
     /**
      * Retorna uma lista de {@link AbstractEmprestavel} através de um {@link LabradorUsuario} que não seja o proprietário.
      * 
-     * @param user
+     * @param user;
      * @return uma lista de {@link AbstractEmprestavel} através de um {@link LabradorUsuario}
      * @throws BeanNotFoundException
      */
@@ -40,4 +47,21 @@ public class AbstractEmprestavelDAO extends BaseDAO<AbstractEmprestavel> {
         critUsuario.add(Restrictions.eq("id", user.getId()));
         return this.executeQuery(c);
     }
+
+    /**
+     * Retorna um {@link AbstractEmprestavel} através de um {@link LabradorUsuario}.
+     * 
+     * @param coisa {@link AbstractEmprestavel} utilizado na consulta
+     * @param labradorUsuario {@link LabradorUsuario} utilizado na consulta
+     * @return uma {@link AbstractEmprestavel} através de um {@link LabradorUsuario}.
+     * @throws BeanNotFoundException
+     */
+    public AbstractEmprestavel findByUsuario(AbstractEmprestavel coisa, LabradorUsuario labradorUsuario) throws BeanNotFoundException {
+        Criteria criteria = this.createCriteria();
+        RestrictionsUtils.addRestrictionEqId(criteria, "id", coisa);
+        Criteria criteriaUsuario = criteria.createCriteria("proprietario");
+        RestrictionsUtils.addRestrictionEqId(criteriaUsuario, "id", labradorUsuario);
+        return this.executeSingleQuery(criteria);
+    }
+
 }
