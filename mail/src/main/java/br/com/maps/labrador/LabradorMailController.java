b6aaca89c0ed7d123dff4de3df678b0417fcc178
fabio.sakiyama <fabio.sakiyama@localhost>
2013-11-08 16:56:34 -0200

    Corrigindo problemas para subir beanfactory do teste de envio de email

--- mail/src/main/java/br/com/maps/labrador/LabradorMailController.java
+++ mail/src/main/java/br/com/maps/labrador/LabradorMailController.java
@@ -1,6 +1,7 @@
 package br.com.maps.labrador;
 
 import jmine.tec.persist.impl.controller.AbstractPersistenceEnabledController;
+import br.com.maps.labrador.mail.actor.EnvioEmailActor;
 
 /**
  * Fachada para o componente labrador-base
@@ -9,5 +10,21 @@ import jmine.tec.persist.impl.controller.AbstractPersistenceEnabledController;
  * @created Nov 07, 2013
  */
 public class LabradorMailController extends AbstractPersistenceEnabledController {
+
+    private EnvioEmailActor envioEmailActor;
+
+    /**
+     * @return the envioEmailActor
+     */
+    public EnvioEmailActor getEnvioEmailActor() {
+        return this.envioEmailActor;
+    }
+
+    /**
+     * @param envioEmailActor the envioEmailActor to set
+     */
+    public void setEnvioEmailActor(EnvioEmailActor envioEmailActor) {
+        this.envioEmailActor = envioEmailActor;
+    }
 
 }
--- mail/src/main/java/br/com/maps/labrador/mail/actor/EnvioEmailActor.java
+++ mail/src/main/java/br/com/maps/labrador/mail/actor/EnvioEmailActor.java
@@ -2,11 +2,32 @@ package br.com.maps.labrador.mail.actor;
 
 import jmine.tec.utils.notifier.mail.MailSender;
 
+/**
+ * Actor responsável por disparar email
+ * 
+ * @author fabio.sakiyama
+ * @date Nov 8, 2013
+ */
 public class EnvioEmailActor {
 
-    private MailSender mailSender = null;
+    private MailSender mailSender;
 
     public void enviaEmail() {
 
     }
+
+    /**
+     * @return the mailSender
+     */
+    public MailSender getMailSender() {
+        return this.mailSender;
+    }
+
+    /**
+     * @param mailSender the mailSender to set
+     */
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
 }
