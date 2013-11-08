6fe2b3ca5366b054ae128896ad3ad1f7e4ed5054
fabio.sakiyama <fabio.sakiyama@localhost>
2013-11-08 16:56:34 -0200

    Corrigindo problemas para subir beanfactory do teste de envio de email

--- mail/src/test/java/br/com/maps/labrador/mail/actor/EmailActorTest.java
+++ mail/src/test/java/br/com/maps/labrador/mail/actor/EmailActorTest.java
@@ -2,22 +2,43 @@ package br.com.maps.labrador.mail.actor;
 
 import jmine.tec.services.impl.testcase.DefaultRefDBTestCase;
 
-public abstract class EmailActorTest extends DefaultRefDBTestCase {
+import org.junit.Test;
 
+/**
+ * Teste do actor {@link EnvioEmailActor}
+ * 
+ * @author fabio.sakiyama
+ * @date Nov 7, 2013
+ */
+public class EmailActorTest extends DefaultRefDBTestCase {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Test
     public void testParametrosMailSender() {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected String getSpringMainXMLFilename() {
         return "mail-beans.xml";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected String[] getResourcesBaseReferencia() {
         return new String[]{ "mail-db.xml" };
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected String getControllerSpringId() {
         return "labradorMailController";
